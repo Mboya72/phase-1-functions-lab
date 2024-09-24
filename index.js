57db47ea7e814ebd04ec3af5b464eb4d8e44d0c6
Mboya72 <elvindio72@gmail.com>
2024-09-24 19:24:36 +0300

    just commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    const hq = '42';
+    if (distance > hq) { return distance - hq }
+    else { return hq - distance }
+}
+function distanceFromHqInFeet(distance) {
+    const hq = '42';
+    if (distance > hq) {
+        return (distance - hq) * 264;
+    } else {
+        return (hq - distance) * 264;
+    }
+}
+function distanceTravelledInFeet(start, destination) {
+    const hq = '42';
+    if (destination > start) {
+        return (destination - start) * 264;
+    } else { return (start - destination) * 264; }
+}
+function calculatesFarePrice( start,destination) {
+    let distance = distanceTravelledInFeet(destination,start)
+    if (distance > 2000 && distance < 2500) {
+        return 25;
+    }
+    else if(distance <= 400) {
+        return 0;
+    }
+    else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * 0.02;
+    }
+
+    else if(distance > 2500){
+        return 'cannot travel that far';
+    }
+}
